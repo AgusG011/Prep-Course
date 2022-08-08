678f340322ea5a02989f61717ecb457dc31f6cc7
AgusG011 <agustinguerrero642@gmail.com>
2022-08-08 12:51:50 -0300

    homework 10

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var nuevoArray = [];
+  for (var clave in objeto) {
+    nuevoArray.push([clave,objeto[clave]]);
+  }
+  return nuevoArray;
 }
 
 
@@ -18,6 +23,16 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var objeto = {}
+  for (let i = 0; i < string.length; i++) {
+    if (objeto.hasOwnProperty(string[i])) {
+      objeto[string[i]] = objeto[string[i]] + 1;
+    }
+    else{
+      objeto[string[i]]=1;
+    }
+  }
+  return objeto;
 }
 
 
@@ -26,6 +41,17 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayus = ""
+  var minus = ""
+  for(let i = 0; i < s.length; i++ ){
+    if(s[i] === s[i].toLowerCase()){
+      minus = minus + s[i];
+    }
+    else{
+      mayus = mayus + s[i];
+    }
+  }
+  return mayus+minus;
 }
 
 
@@ -35,7 +61,13 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+  var nuevoArray = str.split(" ");
+  var finalarray = nuevoArray.map(function(ele)
+  {
+    return ele.split("").reverse().join("");
+  })
+  return finalarray.join("");
+}
 
 
 function capicua(numero){
@@ -43,6 +75,7 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+   
 }
 
 
